0b15b4629de532439e37653c67ac02ca68871107
Jumas-Cola <kbbyfl91@gmail.com>
2018-11-15 03:04:11 +0300

    solved

--- homework02/sudoku.py
+++ homework02/sudoku.py
@@ -25,7 +25,7 @@ def group(values, n):
     >>> group([1,2,3,4,5,6,7,8,9], 3)
     [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
     """
-    pass
+    return [values[i:i+n] for i in [j for j in range(len(values)) if j % n == 0]]
 
 
 def get_row(values, pos):
@@ -38,7 +38,7 @@ def get_row(values, pos):
     >>> get_row([['1', '2', '3'], ['4', '5', '6'], ['.', '8', '9']], (2, 0))
     ['.', '8', '9']
     """
-    pass
+    return values[pos[0]]
 
 
 def get_col(values, pos):
@@ -51,7 +51,7 @@ def get_col(values, pos):
     >>> get_col([['1', '2', '3'], ['4', '5', '6'], ['.', '8', '9']], (0, 2))
     ['3', '6', '9']
     """
-    pass
+    return [i[pos[1]] for i in values]
 
 
 def get_block(values, pos):
@@ -65,7 +65,13 @@ def get_block(values, pos):
     >>> get_block(grid, (8, 8))
     ['2', '8', '.', '.', '.', '5', '.', '7', '9']
     """
-    pass
+    row = (pos[0] // 3) * 3 + 3
+    col = (pos[1] // 3) * 3 + 3
+    res = []
+    for l in [j[col-3 : col] for j in [i for i in values[row-3 : row]]]:
+        for num in l:
+            res.append(num)
+    return res
 
 
 def find_empty_positions(grid):
@@ -78,7 +84,10 @@ def find_empty_positions(grid):
     >>> find_empty_positions([['1', '2', '3'], ['4', '5', '6'], ['.', '8', '9']])
     (2, 0)
     """
-    pass
+    for row in range(len(grid)):
+        for col in range(len(grid[row])):
+            if grid[row][col]=='.':
+                return (row, col)
 
 
 def find_possible_values(grid, pos):
@@ -92,7 +101,11 @@ def find_possible_values(grid, pos):
     >>> values == {'2', '5', '9'}
     True
     """
-    pass
+    pos_val = set('123456789')
+    pos_val-=set(get_row(grid, pos))
+    pos_val-=set(get_col(grid, pos))
+    pos_val-=set(get_block(grid, pos))
+    return pos_val
 
 
 def solve(grid):
@@ -108,13 +121,30 @@ def solve(grid):
     >>> solve(grid)
     [['5', '3', '4', '6', '7', '8', '9', '1', '2'], ['6', '7', '2', '1', '9', '5', '3', '4', '8'], ['1', '9', '8', '3', '4', '2', '5', '6', '7'], ['8', '5', '9', '7', '6', '1', '4', '2', '3'], ['4', '2', '6', '8', '5', '3', '7', '9', '1'], ['7', '1', '3', '9', '2', '4', '8', '5', '6'], ['9', '6', '1', '5', '3', '7', '2', '8', '4'], ['2', '8', '7', '4', '1', '9', '6', '3', '5'], ['3', '4', '5', '2', '8', '6', '1', '7', '9']]
     """
-    pass
+    empty = find_empty_positions(grid)
+    if not empty:
+        return grid
+    else:
+        pos_vals = find_possible_values(grid, empty)
+        for pos_val in pos_vals:
+            grid_1 = [[j for j in i] for i in grid]
+            grid_1[empty[0]][empty[1]] = pos_val
+            res = solve(grid_1)
+            if res:
+                return res
 
 
 def check_solution(solution):
     """ Если решение solution верно, то вернуть True, в противном случае False """
-    # TODO: Add doctests with bad puzzles
-    pass
+    values = set('123456789')
+    for i in range(9):
+        if (set(get_row(solution, (i, 0))) != values):
+            return False
+        if (set(get_col(solution, (0, i))) != values):
+            return False
+        if (set(get_block(solution, (i//3*3, i%3*3))) != values):
+            return False
+    return True
 
 
 def generate_sudoku(N):
@@ -139,7 +169,15 @@ def generate_sudoku(N):
     >>> check_solution(solution)
     True
     """
-    pass
+    sudoku = [['.' for i in range(9)] for j in range(9)]
+    if N > 81:
+        N = 81
+    for i in range(N):
+        index = (random.randint(0,8), random.randint(0,8))
+        while sudoku[index[0]][index[1]] != '.':
+            index = (random.randint(0,8), random.randint(0,8))
+        sudoku[index[0]][index[1]] = random.choice('123456789')
+    return sudoku
 
 
 if __name__ == '__main__':
