600c1ccee29fef31c9e6b1d4368231647b0f5282
Jumas <kbbyfl91@gmail.com>
2019-08-15 14:43:48 +0300

    add rest api

--- homework06/bayes.py
+++ homework06/bayes.py
@@ -1,17 +1,32 @@
+from sklearn.naive_bayes import GaussianNB  # MultinomialNB
+import numpy as np
+
+
 class NaiveBayesClassifier:
 
-    def __init__(self, alpha):
-        pass
+    def __init__(self, alpha=None):
+        self.model = GaussianNB()  # MultinomialNB()
 
     def fit(self, X, y):
         """ Fit Naive Bayes classifier according to X, y. """
-        pass
+        return self.model.fit(X, y)
 
     def predict(self, X):
         """ Perform classification on an array of test vectors X. """
-        pass
+        return self.model.predict(X)
 
     def score(self, X_test, y_test):
         """ Returns the mean accuracy on the given test data and labels. """
-        pass
+        return self.model.score(X_test, y_test)
 
+
+x = np.array([[-3, 7], [1, 5], [1, 2], [-2, 0], [2, 3], [-4, 0],
+              [-1, 1], [1, 1], [-2, 2], [2, 7], [-4, 1], [-2, 7]])
+Y = np.array([3, 3, 3, 3, 4, 3, 3, 4, 3, 4, 4, 4])
+
+model = NaiveBayesClassifier()
+model.fit(x, Y)
+
+# Predict Output
+predicted = model.predict([[1, 2], [3, 4]])
+print(predicted)
---
+++ homework07/djangorest/todolist/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import url, include
+from rest_framework.urlpatterns import format_suffix_patterns
+from .views import TaskCreateView, TaskDetailsView, TasklistCreateView, TasklistDetailsView
+
+
+urlpatterns = {
+    url(r'^todolists/$', TasklistCreateView.as_view(), name="lists"),
+    url(r'^todolists/(?P<pk>[0-9]+)/$', TasklistDetailsView.as_view(), name="list-detail"),
+    url(r'^todolists/(?P<list_id>[0-9]+)/tasks', TaskCreateView.as_view(), name="tasks"),
+    url(r'^todolists/(?P<list_id>[0-9]+)/tasks/(?P<pk>[0-9]+)', TaskDetailsView.as_view(), name="task-detail"),
+}
+
+urlpatterns = format_suffix_patterns(urlpatterns)
